e44196a1254dddf1b0020c4e047682f7735a0842
NoelBrazza <160472950+NoelBrazza@users.noreply.github.com>
2024-05-13 18:03:01 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  MONGODB_URL: process.env.MONGODB_URL || "",
+  MONGODB_URL: process.env.MONGODB_URL || "mongodb+srv://sixom67481:QMF41NMTDOazwOtH@clusterO.toptapq.mongodb.net/",
   SESSION_NAME: process.env.SESSION_NAME || "Bot~"
 };
